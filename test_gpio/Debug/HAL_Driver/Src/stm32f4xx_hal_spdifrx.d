2217b626cfa3cd6893ff35da5c473c494c07c41d
Matej Gomboc <matejg03@gmail.com>
2016-06-28 16:26:13 +0200

    Update

---
+++ test_gpio/Debug/HAL_Driver/Src/stm32f4xx_hal_spdifrx.d
@@ -0,0 +1,182 @@
+HAL_Driver/Src/stm32f4xx_hal_spdifrx.o: \
+ ../HAL_Driver/Src/stm32f4xx_hal_spdifrx.c \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/device/stm32f4xx.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/device/stm32f411xe.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/core/core_cm4.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/core/core_cmInstr.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/core/cmsis_gcc.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/core/core_cmFunc.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/core/core_cmSimd.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/device/system_stm32f4xx.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_adc.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_can.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_crc.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_cryp.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_dac.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_dcmi.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_eth.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_sram.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_nor.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_nand.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_pccard.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_hash.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_iwdg.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_ltdc.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_rng.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_rtc.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_sd.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_ll_sdmmc.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_uart.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_usart.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_irda.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_smartcard.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_wwdg.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_pcd.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h \
+ C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_hcd.h
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_conf.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/device/stm32f4xx.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/device/stm32f411xe.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/core/core_cm4.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/core/core_cmInstr.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/core/cmsis_gcc.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/core/core_cmFunc.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/core/core_cmSimd.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/device/system_stm32f4xx.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_adc.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_can.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_crc.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_cryp.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_dac.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_dcmi.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_eth.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_sram.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_nor.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_nand.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_pccard.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_hash.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_iwdg.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_ltdc.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_rng.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_rtc.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_rtc_ex.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_sd.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_ll_sdmmc.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_usart.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_irda.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_smartcard.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_wwdg.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_pcd.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h:
+
+C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
---
+++ test_gpio/Debug/src/subdir.mk
@@ -0,0 +1,37 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/AccelStepper.c \
+../src/main.c \
+../src/stm32f4xx_it.c \
+../src/syscalls.c \
+../src/system_stm32f4xx.c 
+
+OBJS += \
+./src/AccelStepper.o \
+./src/main.o \
+./src/stm32f4xx_it.o \
+./src/syscalls.o \
+./src/system_stm32f4xx.o 
+
+C_DEPS += \
+./src/AccelStepper.d \
+./src/main.d \
+./src/stm32f4xx_it.d \
+./src/syscalls.d \
+./src/system_stm32f4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32F4 -DSTM32 -DNUCLEO_F411RE -DSTM32F411RETx -DDEBUG -DUSE_HAL_DRIVER -DSTM32F411xE -I"C:/Users/user/Desktop/nucleo_workspace/test_gpio/inc" -I"C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/core" -I"C:/Users/user/Desktop/nucleo_workspace/test_gpio/CMSIS/device" -I"C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc/Legacy" -I"C:/Users/user/Desktop/nucleo_workspace/test_gpio/HAL_Driver/Inc" -I"C:/Users/user/Desktop/nucleo_workspace/test_gpio/Utilities/STM32F4xx-Nucleo" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
